318c9d622288ce67454f558d2a994a16dd6b8557
PeteZah <petezahiscool@gmail.com>
2025-06-25 15:11:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import fileUpload from "express-fileupload";
 import { signupHandler } from "./server/api/signup.js";
 import { signinHandler } from "./server/api/signin.js";
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "production"}` });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const { SUPABASE_URL, SUPABASE_KEY, SUPABASE_SERVICE_ROLE_KEY } = process.env;
